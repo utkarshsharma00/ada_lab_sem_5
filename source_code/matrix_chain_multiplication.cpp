81ec0fa82386b97b6b9f544205453c60f2bdb4fb
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-17 19:25:27 +0530

    Create matrix_chain_multiplication.cpp

---
+++ source_code/matrix_chain_multiplication.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void matrix_chain_multiplication(vector<int> &matrix_arr)
+{
+    vector<vector<int>> storage(matrix_arr.size(), vector<int>(matrix_arr.size() - 1));
+    for (int gap = 0; gap < matrix_arr.size(); gap++)
+    {
+        for (int i = 0, j = i + gap; j < storage.size(); i++, j++)
+        {
+            if (gap == 0)
+            {
+                storage[i][j] = 0;
+            }
+            else if (gap == 1)
+            {
+                if (matrix_arr[i] == matrix_arr[j])
+                {
+                    storage[i][j] = 0;
+                }
+                else
+                {
+                    storage[i][j] = matrix_arr[i] * matrix_arr[j] * matrix_arr[j + 1];
+                }
+            }
+            else
+            {
+                int mymin = INT_MAX;
+                for (int k = 0; k < gap; k++)
+                {
+                    int lhs = storage[i][j - gap + k]; //this is basically the start point
+                    int rhs = storage[i + 1 + k][j];
+                    mymin = min(lhs + rhs + matrix_arr[j - gap] * matrix_arr[j - gap + k + 1] * matrix_arr[j + 1], mymin);
+                }
+
+                storage[i][j] = mymin;
+            }
+        }
+    }
+
+    for (int i = 0; i < storage.size(); i++)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            cout << storage[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> matrix_arr{10, 20, 30, 40, 50, 60};
+    matrix_chain_multiplication(matrix_arr);
+}
