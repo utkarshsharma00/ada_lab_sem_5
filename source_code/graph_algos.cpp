c564f7f55184f513a0039bf6d752b432021a1093
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-22 18:37:05 +0530

    Update graph_algos.cpp

--- source_code/graph_algos.cpp
+++ source_code/graph_algos.cpp
@@ -50,6 +50,72 @@ public:
     }
 };
 
+class Prims_Helper
+{
+public:
+    int vertex;
+    int acquiring_vertex;
+    int cost_of_acquiring;
+
+    Prims_Helper(int vertex, int acquiring_vertex, int cost_of_acquiring)
+    {
+        this->vertex = vertex;
+        this->acquiring_vertex = acquiring_vertex;
+        this->cost_of_acquiring = cost_of_acquiring;
+    }
+
+    bool operator<(const Prims_Helper &other) const
+    {
+        return this->cost_of_acquiring < other.cost_of_acquiring;
+    }
+
+    bool operator>(const Prims_Helper &other) const
+    {
+        return this->cost_of_acquiring > other.cost_of_acquiring;
+    }
+};
+
+class Kruskal_Helper
+{
+public:
+    int vertex_1;
+    int vertex_2;
+    int weight;
+
+    Kruskal_Helper(int vertex_1, int vertex_2, int weight)
+    {
+        this->vertex_1 = vertex_1;
+        this->vertex_2 = vertex_2;
+        this->weight = weight;
+    }
+
+    bool operator>(const Kruskal_Helper &other) const
+    {
+        return this->weight > other.weight;
+    }
+};
+
+class Bellman_Helper
+{
+public:
+    int vertex_1;
+    int vertex_2;
+    int weight;
+
+    Bellman_Helper(int vertex_1, int vertex_2, int weight)
+    {
+        this->vertex_1 = vertex_1;
+        this->vertex_2 = vertex_2;
+        this->weight = weight;
+    }
+
+    bool operator>(const Bellman_Helper &other) const
+    {
+        return this->weight > other.weight;
+    }
+};
+
+
 void addEdge(vector<vector<Edge>> &graph, int vertex1, int vertex2, int weight)
 {
     graph[vertex1].push_back(Edge(vertex2, weight));
