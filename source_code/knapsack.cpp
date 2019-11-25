bf47a039aace22e64d69dd1d6dcb90930df96ab5
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-25 19:06:53 +0530

    Update knapsack.cpp

--- source_code/knapsack.cpp
+++ source_code/knapsack.cpp
@@ -26,3 +26,28 @@ void knapsack(vector<int> &weight, vector<int> &prices, int Capacity)
 
     cout << strg[strg.size() - 1][strg[0].size() - 1] << endl;
 }
+
+void unbounded_knapsack(vector<int> &weight, vector<int> &prices, int Capacity)
+{
+    vector<int> strg(Capacity + 1);
+    for (int i = 1; i <= Capacity; i++)
+    {
+        for (int j = 1; j < weight.size(); j++)
+        {
+            if (i >= weight[j])
+            {
+                strg[i] = max(strg[i - weight[j]] + prices[j], strg[i]);
+            }
+        }
+    }
+
+    cout << strg[Capacity] << endl;
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> weight{2, 5, 1, 3, 4};
+    vector<int> prices{15, 14, 10, 45, 30};
+    knapsack(weight, prices, 7);
+    unbounded_knapsack(weight, prices, 7);
+}
